2d9225bda9b4bc77b3a0cd83e1032ad6815c3e7d
ekiidx <info@vuedesign.co>
2024-05-26 23:01:52 -0400

    added upper and lower map layers and npcs

--- assets/js/world.js
+++ assets/js/world.js
@@ -3,31 +3,40 @@ class World {
         this.element = config.element
         this.canvas = this.element.querySelector(".game-canvas")
         this.ctx = this.canvas.getContext("2d")
+        this.map = null
+    }
+
+    startGameLoop() {
+        const step = () => {
+
+            // Clear the frame
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+
+            // Draw Lower Layer
+            this.map.drawLowerImage(this.ctx)
+
+            // Draw Game Objects
+            Object.values(this.map.gameObjects).forEach(object => {
+                object.update({
+                    arrow: this.directionInput.direction
+                })
+                object.sprite.draw(this.ctx)
+            })
+
+            // Draw Upper Layer
+            this.map.drawUpperImage(this.ctx)
+
+            requestAnimationFrame(() => {
+                step()
+            })
+        }
+        step()
     }
 
     init() {
-       const image = new Image()
-       image.onload = () => {
-        this.ctx.drawImage(image, 0, 0)
-       }
-       image.src = "/../assets/images/maps/test.png"
-
-       // Place some game objects here
-       const hero = new GameObject({
-        x: 5,
-        y: 6,
-       })
-
-       const npc1 = new GameObject({
-        x: 7,
-        y: 9,
-        src: "/../assets/images/characters/npc1.png"
-       })
-
-       setTimeout(() => {
-        hero.sprite.draw(this.ctx)
-        npc1.sprite.draw(this.ctx)
-       }, 200)
-
+        this.map = new WorldMap(window.WorldMaps.TestRoom)
+        this.directionInput = new PlayerInput()
+        this.directionInput.init()
+        this.startGameLoop()
     }
 }

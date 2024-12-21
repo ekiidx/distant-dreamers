40cff0e7db9ebb633ece617f75ca9e0803c75755
ekiidx <info@vuedesign.co>
2024-12-21 10:00:00 +0600

    added multiple event spaces

--- js/Actionable.js
+++ js/Actionable.js
@@ -1,31 +1,11 @@
 class Actionable extends GameObject {
     constructor(config) {
         super(config)
+        this.multiSpaceEvent = config.multiSpaceEvent;
+        this.matchX = config.matchX;
+        this.matchY = config.matchY;
         this.actions = config.actions;
-        // this.sprite = new Sprite({
-        //     gameObject: this,
-        //     src: "",
-        // });
-        // this.storyFlag = config.storyFlag;
-        // this.item = config.item;
-
-        // this.actions = [
-        //     // {
-        //     //     required: [this.storyFlag],
-        //     //     events: [
-        //     //        { type: "message", text: "Already opened." },
-        //     //     ]
-        //     // },
-        //     // {
-        //     //     events: [
-        //     //         { type: "message", text: "You found an actionable!" },
-        //     //     ]
-        //     // }
-        // ]
     }
-
     update() {
-        // this.sprite.currentAnimation = playerState.storyFlags[this.storyFlag]
-        // ? "chest-open" : "chest-closed";
     }
 }
